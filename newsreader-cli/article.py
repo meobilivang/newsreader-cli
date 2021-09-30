a92fe353f7d9ea12d46cd068dd879651776e0362
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-09-30 02:13:23 -0400

    feat: scraping news from main page

--- newsreader-cli/article.py
+++ newsreader-cli/article.py
@@ -1,2 +1,19 @@
 class Article:
-    def __init__(self):
+    def __init__(self, title = "Empty Title", url = "Empty URL", description = "Empty Description", content = "Empty Content"):
+        self.title = title
+        self.url = url
+        self.description = description
+        self.content = content
+
+    def __get_title__(self):
+        return self.title
+    
+    def __get_url__(self):
+        return self.url
+    
+    def __get_description__(self):
+        return self.description
+    
+    def __get_content__(self):
+        return self.content
+    
--- newsreader-cli/scraper.py
+++ newsreader-cli/scraper.py
@@ -1,20 +1,32 @@
 import requests
+
 from bs4 import BeautifulSoup
+from article import Article
+
+def text_convert(input):
+    """
+        input: raw scraped html
+    """
+    return "" if input is None else input.get_text().strip()
 
 URL = 'https://vnexpress.net/'
-
 page = requests.get(URL).text
 
 soup = BeautifulSoup(page, "html.parser")
 
 article_list_raw = soup.find_all('article')
+article_list = []
 
 for article_raw in article_list_raw:
-    headline = article_raw.h3.get_text().strip()
-    url = article
-    desc = "" if article_raw.p.get_text() is None else article_raw.p.get_text().strip()
-    print(headline)
-    print(desc)
+    article_list.append(Article(
+        text_convert(article_raw.h3.a), 
+        article_raw.h3.a['href'], 
+        text_convert(article_raw.p)
+        ))
+
+    print(article_list[-1].__get_title__())
+    print(article_list[-1].__get_url__())
+    print(article_list[-1].__get_description__())
     print('----------')
 
 
