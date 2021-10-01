d7d059a5e5a2252baa3a85f2f03010cd9eb4b1df
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-10-01 10:05:38 -0400

    feat: detail article read

--- newsreader-cli/article.py
+++ newsreader-cli/article.py
@@ -1,9 +1,14 @@
+from typing import Text
+
+
 class Article:
-    def __init__(self, title = "Empty Title", url = "Empty URL", description = "Empty Description", content = "Empty Content"):
+    def __init__(self, title = "Empty Title", url = "Empty URL", description = "Empty Description", text_content = [], date = "Unidentified", category = []):
         self.title = title
         self.url = url
         self.description = description
-        self.content = content
+        self.text_content = text_content
+        self.date = date
+        self.catergory = category
 
     def __get_title__(self):
         return self.title
@@ -15,5 +20,22 @@ class Article:
         return self.description
     
     def __get_content__(self):
-        return self.content
+        return self.text_content
+
+    def __get_date__(self):
+        return self.date
+    
+    def __get_catergory(self):
+        return self.category
+    
+    def __set_date(self, date):
+        self.date = date
+
+    def __insert_text_content_(self, new_text_content):
+        self.text_content.append(new_text_content)
+    
+    
+    def __insert_category(self, new_category):
+        self.category.insert(new_category)
+        
     
--- newsreader-cli/scraper.py
+++ newsreader-cli/scraper.py
@@ -9,25 +9,60 @@ def text_convert(input):
     """
     return "" if input is None else input.get_text().strip()
 
-URL = 'https://vnexpress.net/'
-page = requests.get(URL).text
+BASE_URL = 'https://vnexpress.net/'
+page = requests.get(BASE_URL).text
 
 soup = BeautifulSoup(page, "html.parser")
 
 article_list_raw = soup.find_all('article')
 article_list = []
+counter = 0
 
 for article_raw in article_list_raw:
+    
     article_list.append(Article(
         text_convert(article_raw.h3.a), 
         article_raw.h3.a['href'], 
         text_convert(article_raw.p)
-        ))
+    ))
+    
+    counter += 1
 
-    print(article_list[-1].__get_title__())
-    print(article_list[-1].__get_url__())
-    print(article_list[-1].__get_description__())
-    print('----------')
+    #print(counter)
+    # print(article_list.__get_title__())
+    # print(article_list.__get_url__())
+    # print(article_list.__get_description__())
+    #print('----------')
 
+def make_soup(url = None):
+    page = requests.get(BASE_URL if url is None else url).text
+    return BeautifulSoup(page, "html.parser")
 
+def article_scrape(article):
+    soup = make_soup(article.__get_url__())
+    
+    # article_header = soup.find_all("div", class_="header_conent") 
+    
+    # for tag in article_header:
+    #     article_date = tag.find_all("span", class_="date")
+    #     print(type(article_date))
+    #     article_category_list = tag.find_all("li")
+    #     print(article_category_list)
+
+    #for each_category in article_category_list:
+        #article.__insert_category(each_category)
+
+    article_text_content_list_raw = soup.find_all('p', class_="Normal")
+    #print(article_text_content_list_raw)
+    for tag in article_text_content_list_raw:
+        print(tag)
+
+    # for each_paragraph in article_text_content_list:
+    #     article.__insert_text_content(each_paragraph)
+    #     print(article_text_content_list)
+
+    return
+
+print(article_list[0].url)
+article_scrape(article_list[0])
 
