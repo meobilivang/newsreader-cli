7e1b07a34e5ee40d82ec62124a53893e1e9ba3ba
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-10-29 02:53:17 -0400

    ref: dir change

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -7,7 +7,7 @@ def test_web_scraper():
     
     scraper = NewsScraper()
 
-    test_page = open("tests/vnexpress-test.html", 'r', encoding='utf-8')
+    test_page = open("tests/test_data/vnexpress-test.html", 'r', encoding='utf-8')
     scraper.page_scrape(None,  test_page.read())
     
     for article in scraper.articles_current_page:
